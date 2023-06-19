94dfb8ba780ca515ced605cb823344b8058ef06c
Hesham <heshamsharafaldin@gmail.com>
2023-06-19 17:06:26 -0400

    in progress: debugging atm

--- assignment6/src/components/Table.jsx
+++ assignment6/src/components/Table.jsx
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import TableRow from "./TableRow";
 
  const Table = () => {
     const [grid, setGrid] = useState([[""]]);
+    const [selectedColor, setSelectedColor] = useState("");
+    
     const addRow = () => {
         var newRow = new Array(grid[0]?.length || 0).fill("");
         setGrid(prevState => [...prevState, newRow]);
@@ -15,27 +17,47 @@ import TableRow from "./TableRow";
         });
           
     }
+            const selectColor = (event) => {
+                setSelectedColor(event.target.value);
+                console.log(selectedColor);
+                
+            }
+        
+        
+    
 
+    const clickCell = (event) => {
+        const color = selectedColor;
+        event.target.style.backgroundColor = color;
+      };
+
+    /*
+    const selectColor = (e) => {
+        setSelectd(e.target.value);
+    }*/
+    
  
     return (
         <div>
+            <div>
+                {/* onClick ={clickCell} */}
+            </div>
          <table>
             <tbody>
              {grid.map((row, index) => (
-                  <TableRow key={index} rowData={row} />   
+                  <TableRow key={index} rowData={row} color={selectedColor}/>   
              ))}
             </tbody>
          </table>
              <button onClick={addRow}>Add Row</button>
              <button onClick={addColumn}>Add Column</button> 
-             <label for="colorSelect">Select a color:</label>
-            <select id="colorSelect" onchange="getColor()">
-            <option value="null">---</option>
-            <option value="red">Red</option>
-            <option value="green">Green</option>
-            <option value="blue">Blue</option>
-            <option value="yellow">Yellow</option>
-            <option value="orange">Orange</option>
+             <label >Select a color:</label>
+            <select  value={selectedColor} onChange={() => selectColor(event)}>
+                <option value="red">Red</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+                <option value="yellow">Yellow</option>
+                <option value="orange">Orange</option>
             </select>
         </div>
      );
--- assignment6/src/components/TableRow.jsx
+++ assignment6/src/components/TableRow.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import TableCell from './TableCell';
 
-const TableRow = ({rowData}) => {
+const TableRow = ({rowData, selectedColor}) => {
   
     return (
       <tr>
         {rowData.map((index, content) => (
-          <TableCell key={index} content={""} />
+          <TableCell key={index} content={""} selectedColor={selectedColor} />
         ))}
       </tr>
     );
